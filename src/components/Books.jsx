b88092862094d1170127abf572ad7ed999aaaa18
sakibmahbub <arafatsakib48@gmail.com>
2023-04-08 00:40:59 +0600

    fetched data from book api

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Books = () => {
+  const {books} = useLoaderData();
+  console.log(books);
   return <div></div>;
 };
 
